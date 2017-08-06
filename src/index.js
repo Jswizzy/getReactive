bfac9f85488d14106f6074ee263ab7aa0957e318
Jswizzy <smithjustinn@hotmail.com>
2017-08-06 11:50:59 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import Timer from "./Timer";
+import TodoApp from "./TodoApp";
+import Button from "./Counter";
+import App1 from "./Counter"
+
+ReactDOM.render(<App1 />, document.getElementById('root'));
+registerServiceWorker();
